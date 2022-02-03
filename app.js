6450cafc02b3ce6352af600c2909ad2687a6a712
Kathryn Hodgson <k3hdgsn@gmail.com>
2022-02-03 15:37:43 +0000

    update no images bug

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const dogCard = document.getElementById('dogCard');
 
 const fetchDog = async () => {
-    const url = `https://pokeapi.co/api/v2/pokemon?limit=150`;
+    const url = `https://dog.ceo/api/breeds/image/random`;
     const res = await fetch(url);
     const data = await res.json();
         
     const dog = data.results.map((data, index) => ({
         name: data.name,
         id: index + 1,
-        image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index +
+        image: `https://dog.ceo/api/breeds/image/random/${index +
         1}.png`
     }));
     
