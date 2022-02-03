d7d8331dcf6d5bbebce4191e1a51c062a048b920
Kathryn Hodgson <k3hdgsn@gmail.com>
2022-02-03 15:31:53 +0000

    debug map() errors

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const dogCard = document.getElementById('dogCard');
 
 const fetchDog = async () => {
-    const url = `https://dog.ceo/api/breeds/list/all`;
+    const url = `https://pokeapi.co/api/v2/pokemon?limit=150`;
     const res = await fetch(url);
     const data = await res.json();
         
     const dog = data.results.map((data, index) => ({
         name: data.name,
         id: index + 1,
-        image: `https://dog.ceo/api/breeds/image/random/${index + 1}`  
+        image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index +
+        1}.png`
     }));
     
     displayDog(dog); 
