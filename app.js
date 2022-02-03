d1daaca0728b7fc8bb33d5a2d4b614a61bf0c24a
Kathryn Hodgson <k3hdgsn@gmail.com>
2022-02-03 15:30:23 +0000

    debug map() error

--- app.js
+++ app.js
@@ -1,25 +1,20 @@
 const dogCard = document.getElementById('dogCard');
 
 const fetchDog = async () => {
-    console.log('fetch get async')
-
-    try{
-        const url = `https://dog.ceo/api/breeds/list/all`;
-        const res = await fetch(url);
-        const data = await res.json();
+    const url = `https://dog.ceo/api/breeds/list/all`;
+    const res = await fetch(url);
+    const data = await res.json();
         
-        const dog = data.results.map((data, index) => ({
-            name: data.name,
-            id: index + 1,
-            image: `https://dog.ceo/api/breeds/image/random/${index + 1}`  
-        }));
-        console.log('here');
-        displayDog(dog); 
-    }catch(err){
-        console.log(err);
-    }
+    const dog = data.results.map((data, index) => ({
+        name: data.name,
+        id: index + 1,
+        image: `https://dog.ceo/api/breeds/image/random/${index + 1}`  
+    }));
     
+    displayDog(dog); 
 };
+
+/*
 
 const displayDog = (dog) => {
     const dogHTMLString = dog
@@ -33,6 +28,6 @@ const displayDog = (dog) => {
         )
         .join('');
     dogCard.innerHTML = dogHTMLString;
-};
+}; */
 
 fetchDog();
