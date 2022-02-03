eb024c72ad5875d8d32aea929d370b5e53fbeaed
Kathryn Hodgson <k3hdgsn@gmail.com>
2022-02-03 10:42:39 +0000

    add inital code

---
+++ app.js
@@ -0,0 +1,28 @@
+const dogCard = document.getElementById('dogCard');
+
+const fetchDog = async () => {
+    const url = ``;
+    const res = await fetch(url);
+    const data = await res.json();
+    const dog = data.results.map((data, index) => ({
+        name: data.name,
+        id: index + 1,
+        image: `https://dog.ceo/api/breeds/image/random`
+    }));
+
+    displayDog(dog);
+};
+
+const displayDog = (dog) => {
+    const dogHTMLString = dog
+        .map(
+            (dog) =>
+                `
+    <li class="card" onclick="selectPokemon(${dog.id})">
+        <img class="card-image" src="${dog.image}"/>
+    </li>
+        `
+        )
+        .join('');
+    dogCard.innerHTML = dogHTMLString;
+};
