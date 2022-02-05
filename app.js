23c220759fa690b7ac867c70f54ee7a5461ae717
Kathryn Hodgson <k3hdgsn@gmail.com>
2022-02-05 12:48:24 +0000

    rewrite js code

---
+++ app.js
@@ -0,0 +1,28 @@
+const dogCard = document.getElementById('dogCard');
+
+const fetchDog = async () => {
+    const url = 'https://dog.ceo/api/breeds/list/all';
+    const res = await fetch(url);
+    const data = await res.json();
+    const dog = data.results.map((data) => ({
+        name: data.name,
+        image: 'https://dog.ceo/api/breeds/image/random'
+    }));
+
+    displayDog(dog);
+}
+
+const displayDog = (dog) => {
+    const dogHTMLString = dog
+        .map(
+            (doggo) =>
+            `
+            <img class="card-image" src="${doggo.image}"/>
+            <p class="card-name">${doggo.name}</p>
+            `
+            )
+            .join('');
+        dogCard.innerHTML = dogHTMLString;
+}
+
+fetchDog();
