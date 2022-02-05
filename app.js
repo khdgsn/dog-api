ee9ae61ee715069a80be39840ea1bedab3eb323e
Kathryn Hodgson <k3hdgsn@gmail.com>
2022-02-05 17:59:58 +0000

    debug errors and update code

--- app.js
+++ app.js
@@ -1,32 +1,36 @@
+const G = require("glob");
+
 // create variable to grab element to change
 const dogCard = document.getElementById('dogCard');
 
 // create variable to grab data from an API
 // use async & await to make javascript wait until program is ready
 const fetchDog = async () => {
+    // assigning varible
     const url = 'https://dog.ceo/api/breeds/image/random';
+    // making js wait until all data us fetched using above variable
     const res = await fetch(url);
+    // tells us that the data is a response stream
+    // assining variable to res so that json can turn it to a JS object
     const data = await res.json();
+    console.log(data);
     
-    const dog = data.results.map((data) => ({
-        image: data.image
-    }));
+    const dog = data.message
+    console.log(dog)
     displayDog(dog);
+
+    var result = url.slice(-1)
+    console.log(result)
 }
 
 const displayDog = (dog) => {
-    const dogHTMLString = dog
-        // creates a new array populated with the results of calling dog on every element in the calling array.
-        .map(
-            (doggo) =>
-            `
-            <img class="card-image" src="${doggo.image}"/>
-            <p class="card-name">${doggo.name}</p>
-            `
-            )
-            // joins populated array together as a string
-            .join('');
-        dogCard.innerHTML = dogHTMLString;
+    const dogHTMLString = `<img class="card-image" src=${dog} />`
+    dogCard.innerHTML = dogHTMLString;
 }
 
 fetchDog();
+
+/*
+how to check last element in string 
+if it doesnt not equal G, remove it
+*/
